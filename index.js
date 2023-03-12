f86be1afafb30dd4a2944320b78dd28ec58e5f8c
Clay Blackwelder <clayblackwelder@gmail.com>
2023-03-11 23:31:29 -0800

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(name, type) {
+    let message = [];
+
+    for (let i = 0; i < name.length; i++) {
+      message.push (`Thank you, ${name[i]}, for the wonderful ${type} gift!`);
+    }
+
+    return message;
+}
+
+function countDown(i) {
+    let downCount = i;
+    while (downCount >= 0)  {
+        console.log(downCount--);
+    }
+}
